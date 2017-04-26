f0b777267b63c3774cbe45bc655b4609772197bb
GOMEL\Aliaksandr_Matveyeu <Aliaksandr_Matveyeu@work.com>
2017-04-26 21:36:36 +0300

    Improved exit when getting exception

--- boto3/boto3stack.py
+++ boto3/boto3stack.py
@@ -1,17 +1,17 @@
 #! /usr/bin/env python
 # import boto3 library
 import boto3
-from sys import exc_info
+from sys import exit
 
 def create_stack():
     """Creates aws cloudformation stack from file"""
     # check if file exists
     try:
         # open template file
-        template_path = open('SampleNetworkCrossStack.json')
+        template_path = open('SampleNetworkCrossStack1.json')
     except (OSError, IOError) as err:
         print("Error: {}".format(err))
-        return 0
+        exit(1)  # or return 0
     else:
         # read template file
         read_template = template_path.read()
