3266e574a5d20b0e24baf21e6d50d30cf5679197
GOMEL\Aliaksandr_Matveyeu <Aliaksandr_Matveyeu@work.com>
2017-04-26 22:53:39 +0300

    Added checking if stack already exists

--- boto3/boto3stack.py
+++ boto3/boto3stack.py
@@ -2,39 +2,55 @@
 # import boto3 library
 import boto3
 from sys import exit
+from botocore.exceptions import BotoCoreError, ClientError
+
+template_file = 'SampleNetworkCrossStack.json'
+stackname = 'TestStack'
 
 def create_stack():
     """Creates aws cloudformation stack from file"""
     # check if file exists
     try:
         # open template file
-        template_path = open('SampleNetworkCrossStack1.json')
+        template_opened = open(template_file)
     except (OSError, IOError) as err:
         print("Error: {}".format(err))
         exit(1)  # or return 0
     else:
         # read template file
-        read_template = template_path.read()
-        template_path.close()
+        read_template = template_opened.read()
+        template_opened.close()
 
     # add cf client
     client = boto3.client('cloudformation')
 
-    # create stack
-    stack = client.create_stack(
-        StackName='TestStack',
-        TemplateBody=read_template,
-        Capabilities=[
-            'CAPABILITY_IAM',
-            'CAPABILITY_NAMED_IAM',
-        ]
-    )
+    # check if stack exists
+    try:
+        stack_exists = client.describe_stacks(
+            StackName=stackname
+        )
+    except ClientError:
+        # create stack
+        stack = client.create_stack(
+            StackName=stackname,
+            TemplateBody=read_template,
+            Capabilities=[
+                'CAPABILITY_IAM',
+                'CAPABILITY_NAMED_IAM',
+            ]
+        )
 
-    # add waiter
-    waiter = client.get_waiter('stack_create_complete')
+        # add waiter
+        waiter = client.get_waiter('stack_create_complete')
 
-    # wait until stack would be created
-    waiter.wait(StackName='TestStack')
+        # wait until stack would be created
+        waiter.wait(StackName='TestStack')
+    except BotoCoreError as core_except:
+        print("Core Error: []".format(core_except))
+        exit(1)
+    else:
+        print("Error: Stack \"{}\" already exists".format(stackname))
+        exit(1)
 
 if __name__ == '__main__':
     create_stack()
