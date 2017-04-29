9cb296db1410bdd00c64cea4fca3bcbdeea619d4
GOMEL\Aliaksandr_Matveyeu <Aliaksandr_Matveyeu@work.com>
2017-04-29 09:45:10 +0300

    code refactoring

--- task4/stack_wrapper.py
+++ task4/stack_wrapper.py
@@ -5,12 +5,28 @@ import argparse
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError, WaiterConfigError, WaiterError
 
+# for loging configuration
+LOGFORMAT = '%(asctime)s | %(levelname)s | %(funcName)s | %(message)s'
+DATEFORMAT = '%m/%d/%Y %I:%M:%S %p'
+# for boto3 stack functions
+ACTION_CREATE = 'create'
+ACTION_UPDATE = 'update'
+ACTION_DELETE = 'delete'
+# types for setting waiters in waiter function
+WAITERS = {
+    ACTION_CREATE: 'stack_create_complete',
+    ACTION_UPDATE: 'stack_update_complete',
+    ACTION_DELETE: 'stack_delete_complete',
+}
 logger = logging.getLogger(__name__)
+client = boto3.client('cloudformation')
 
 
-# Configuring argparse
 def get_args():
-    """Parsing CLI commands"""
+    """Parsing CLI commands using argparse with subparsers for common stack functions.
+    Dublication of log and logfile arguments, because subparsers are using
+    """
+
     parser = argparse.ArgumentParser(description='AWS stack wrapper')
     subparsers = parser.add_subparsers()
 
@@ -20,9 +36,10 @@ def get_args():
     parser_create.add_argument('file', help='set file path')
     parser_create.add_argument('--log',
                                type=str,
-                               default="INFO",
+                               default='INFO',
+                               choices=['INFO', 'DEBUG', 'ERROR'],
                                required=False,
-                               help='which log level. DEBUG, INFO, WARNING, CRITICAL')
+                               help='which log level. DEBUG, INFO, ERROR')
     parser_create.add_argument('--logfile',
                                required=False,
                                default=None,
@@ -35,9 +52,10 @@ def get_args():
     parser_update.add_argument('file', help='set file path')
     parser_update.add_argument('--log',
                                type=str,
-                               default="INFO",
+                               default='INFO',
+                               choices=['INFO', 'DEBUG', 'ERROR'],
                                required=False,
-                               help='which log level. DEBUG, INFO, WARNING, CRITICAL')
+                               help='which log level. DEBUG, INFO, ERROR')
     parser_update.add_argument('--logfile',
                                required=False,
                                default=None,
@@ -49,15 +67,15 @@ def get_args():
     parser_delete.add_argument('stack_name', help='set stack name')
     parser_delete.add_argument('--log',
                                type=str,
-                               default="INFO",
+                               default='INFO',
+                               choices=['INFO', 'DEBUG', 'ERROR'],
                                required=False,
-                               help='which log level. DEBUG, INFO, WARNING, CRITICAL')
+                               help='which log level. DEBUG, INFO, ERROR')
     parser_delete.add_argument('--logfile',
                                required=False,
                                default=None,
                                help='write log to file')
     parser_delete.set_defaults(func=delete_stack)
-
     # if no arguments, show help
     if len(argv) == 1:
         parser.print_help()
@@ -65,29 +83,17 @@ def get_args():
     return parser.parse_args()
 
 
-# add cf client
-client = boto3.client('cloudformation')
-
-
-# try open and read a file
 def open_file(file_name):
-    """opens, reads and validates a file. Checks if the file exists"""
+    """try to open and read cloudformation template file"""
+
     try:
         # open template file
         template_opened = open(file_name)
-
         # read template file
         read_template = template_opened.read()
 
-        # validate template file
-        validate_template = client.validate_template(
-            TemplateBody=read_template,
-        )
     except (OSError, IOError) as error:
         logger.error("I/O Error: {error_message}".format(error_message=error))
-        exit(1)
-    except (BotoCoreError, ClientError) as error:
-        logger.error("Validate Error: {error_message}".format(error_message=error))
         exit(1)
     else:
         logger.info("Template \"{file}\" is valid".format(file=file_name))
@@ -95,131 +101,115 @@ def open_file(file_name):
         return read_template
 
 
-# existing stack function
+def validate_file(read_file):
+    """validate cloudformation template file"""
+    validate_template = client.validate_template(
+        TemplateBody=read_file,
+    )
+
+
 def stack_exists(stackname):
     """Check if stack exists"""
-    try:
-        stack_exists = client.describe_stacks(
-            StackName=stackname
-        )
-    except (BotoCoreError, ClientError) as error:
-        logger.error("Existing Error: {error_message}".format(error_message=error))
-        exit(1)
-    else:
-        logger.info("Stack \"{stack}\" exists".format(stack=stackname))
+
+    stack_exists = client.describe_stacks(
+        StackName=stackname
+    )
+    logger.info("Stack \"{stack}\" exists".format(stack=stackname))
 
 
-# create waiter function
 def set_waiter(stackname, waiter_type):
     """set waiter for boto3 operations"""
 
     try:
         # add waiter
-        waiter = client.get_waiter(waiter_type)
-
+        waiter = client.get_waiter(WAITERS[waiter_type])
         # wait until stack would be updated
         waiter.wait(StackName=stackname)
+    except KeyError as error:
+        logger.error("Key Error: {error_message}".format(error_message=error))
+        exit(1)
     except (WaiterError, WaiterConfigError) as error:
         logger.error("Waiter Error: {error_message}".format(error_message=error))
         exit(1)
     else:
-        logger.info("Stack \"{stack}\" get status:  {status}".format(stack=stackname, status=waiter_type))
+        logger.info("{operation} stack \"{stack}\": {status}".format(operation=waiter_type,
+                                                                     stack=stackname,
+                                                                     status=WAITERS[waiter_type]))
 
 
-# Create stack function
 def create_stack(args):
-    """Creates aws cloudformation stack from file"""
+    """Reads and validates cloudformation template file,
+    Then creates aws cloudformation stack from this template.
+    Finally, set waiter using function with constant action
+    """
 
-    # get read template
     read_template = open_file(args.file)
-
-    # try create stack
-    try:
-        created_stack = client.create_stack(
-            StackName=args.stack_name,
-            TemplateBody=read_template,
-            Capabilities=[
-                'CAPABILITY_IAM',
-                'CAPABILITY_NAMED_IAM',
-            ]
-        )
-        logger.debug("Create stack request: {request}".format(request=created_stack))
-    except (BotoCoreError, ClientError) as error:
-        logger.error("Update Error: {error_message}".format(error_message=error))
-        exit(1)
-
-    # set waiter
-    set_waiter(args.stack_name, 'stack_create_complete')
+    validate_file(read_template)
+    created_stack = client.create_stack(
+        StackName=args.stack_name,
+        TemplateBody=read_template,
+        Capabilities=[
+            'CAPABILITY_IAM',
+            'CAPABILITY_NAMED_IAM',
+        ]
+    )
+    logger.debug("Create stack request: {request}".format(request=created_stack))
+    set_waiter(args.stack_name, ACTION_CREATE)
 
 
-# update stack function
 def update_stack(args):
-    """Updates aws cloudformation stack from file"""
+    """Reads and validates cloudformation template file,
+    then updates aws cloudformation stack from this template.
+    Finally, set waiter using function with constant action
+    """
 
-    # get read template
     read_template = open_file(args.file)
-
-    # try update stack
-    try:
-        updated_stack = client.update_stack(
-            StackName=args.stack_name,
-            TemplateBody=read_template,
-            Capabilities=[
-                'CAPABILITY_IAM',
-                'CAPABILITY_NAMED_IAM',
-            ]
-        )
-        logger.debug("Update stack request: {request}".format(request=updated_stack))
-    except (BotoCoreError, ClientError) as error:
-        logger.error("Update Error: {error_message}".format(error_message=error))
-        exit(1)
-
-    # set waiter
-    set_waiter(args.stack_name, 'stack_update_complete')
+    validate_file(read_template)
+    updated_stack = client.update_stack(
+        StackName=args.stack_name,
+        TemplateBody=read_template,
+        Capabilities=[
+            'CAPABILITY_IAM',
+            'CAPABILITY_NAMED_IAM',
+        ]
+    )
+    logger.debug("Update stack request: {request}".format(request=updated_stack))
+    set_waiter(args.stack_name, ACTION_UPDATE)
 
 
-# delete stack function
 def delete_stack(args):
-    """Deletes aws cloudformation stack"""
-    # check if stack exists
+    """Checks if aws cloudformation stack exists,
+    then deletes this stack.
+    Finally, set waiter using function with constant action
+    """
+
     stack_exists(args.stack_name)
-
-    # try delete stack
-    try:
-        deleted_stack = client.delete_stack(
-            StackName=args.stack_name,
-        )
-        logger.debug("Delete stack request: {request}".format(request=deleted_stack))
-    except (BotoCoreError, ClientError) as error:
-        logger.error("Update Error: {error_message}".format(error_message=error))
-        exit(1)
-    # set waiter
-    set_waiter(args.stack_name, 'stack_delete_complete')
+    deleted_stack = client.delete_stack(
+        StackName=args.stack_name,
+    )
+    logger.debug("Delete stack request: {request}".format(request=deleted_stack))
+    set_waiter(args.stack_name, ACTION_DELETE)
 
 
-# set entry point
 def main():
-    """ entry point """
-    args = get_args()
-    # handle log parameter
-    loglevel = args.log
-    # for passing to logging config
-    numeric_level = getattr(logging, loglevel.upper(), None)
-    # check any user input value of log parameter
-    if not isinstance(numeric_level, int):
-        raise ValueError('Invalid log level: %s' % loglevel)
-    # write log to file
-    if args.logfile:
-        logging.basicConfig(filename=args.logfile,
-                            level=numeric_level,
-                            format='%(asctime)s | %(levelname)-10s %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
-    else:
-        logging.basicConfig(level=numeric_level,
-                            format='%(asctime)s | %(levelname)-10s %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
-    args.func(args)
+    """ entry point: getting arguments,
+    handling log level argument for logging config,
+    configuring logging config and
+    handling exceptions of functions, which used those arguments    
+    """
 
+    args = get_args()
+    loglevel = args.log
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    logging.basicConfig(filename=args.logfile,
+                        level=numeric_level,
+                        format=LOGFORMAT,
+                        datefmt=DATEFORMAT)
+    try:
+        args.func(args)
+    except (BotoCoreError, ClientError) as error:
+        logger.error("Error: {error_message}".format(error_message=error))
+        exit(1)
 
 if __name__ == '__main__':
     main()
